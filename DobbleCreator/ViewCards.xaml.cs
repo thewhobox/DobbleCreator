d6598c35bf4c4a5601dd08f32fe3316bcaae6aed
Mike Gerst <admin@mikegerst.de>
2022-01-14 15:48:34 +0100

    Added PDF output

--- DobbleCreator/ViewCards.xaml.cs
+++ DobbleCreator/ViewCards.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 
 namespace DobbleCreator
 {
@@ -50,23 +55,10 @@ namespace DobbleCreator
             Random rnd = new Random();
             foreach(Card card in cards)
             {
-                /*bool flag = false;
-
-                foreach (int numb in card.Numbers)
-                    if (numb > maxSymbols)
-                        flag = true;
-
-                if (flag)
-                {
-                    fails++;
-                    continue;
-                }
-                */
-
                 CardView newCard = new CardView();
                 card.Numbers.Shuffle();
                 foreach (int numb in card.Numbers) {
-                    double scale = 1.0;
+                    double scale = rnd.Next(7,12) / 10.0;
                     int rotation = rnd.Next(0, 359);
                     newCard.Images.Add(new ImageInfo(images[numb], rotation, scale));
                 }
@@ -77,6 +69,66 @@ namespace DobbleCreator
             {
                 MessageBox.Show("Es konnten " + fails + " Karten nicht erstellt werden...", "Fehler 0x08");
             }
+        }
+
+        private async void DoExport(object sender, RoutedEventArgs e) {
+            ProgExport.Maximum = Cards.Count();
+            ProgExport.Value = 0;
+
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
+
+            if(!System.IO.Directory.Exists("temp")) {
+                System.IO.Directory.CreateDirectory("temp");
+            }
+
+            for(int i = 0; i < Cards.Count; i++) {
+                if(i == 110) break;
+
+                PdfPage page = document.AddPage();
+                page.Width = "65mm"; //223;
+                page.Height = "97mm"; //344;
+
+                RenderTargetBitmap renderTarget = new RenderTargetBitmap(446*3, 666*3, 300, 300, PixelFormats.Pbgra32);
+                VisualBrush sourceBrush = new VisualBrush(cardList.ItemContainerGenerator.ContainerFromIndex(i) as ListBoxItem);
+                DrawingVisual drawingVisual = new DrawingVisual();
+                DrawingContext drawingContext = drawingVisual.RenderOpen();
+                drawingContext.DrawRectangle(sourceBrush, null, new Rect(new Point(0, 0), new Point(446, 666446*1,492)));
+                drawingContext.Close();
+
+                renderTarget.Render(drawingVisual);
+
+                byte[] imageArray;
+                using(FileStream stream = new FileStream($"temp/{i}.jpg", FileMode.OpenOrCreate)){
+                    JpegBitmapEncoder jpgEncoder = new JpegBitmapEncoder();
+                    jpgEncoder.QualityLevel = 100;
+                    jpgEncoder.Frames.Add(BitmapFrame.Create(renderTarget));
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        jpgEncoder.Save(outputStream);
+                        imageArray = outputStream.ToArray();
+                    }
+                    foreach (byte digit in imageArray)
+                    {
+                        stream.WriteByte(digit);
+                    }
+                    stream.Close();
+                }
+
+                XGraphics gfx = XGraphics.FromPdfPage(page);
+                XImage image = XImage.FromFile($"temp/{i}.jpg");
+                gfx.DrawImage(image, 0, 0, 180, 269);
+
+
+                ProgExport.Value = i+1;
+                await System.Threading.Tasks.Task.Delay(10);
+            }
+
+
+
+
+
+            document.Save("test.pdf");
         }
     }
 }
